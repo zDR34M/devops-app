ad9eb687da3d68258d9c06dfc54b2d62d95d254e
Tareq <tareq.cr07@gmail.com>
2025-11-14 21:03:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h1>Welcome to Tareq's DevOps App 🚀</h1>"
+@app.route("/")
+def hello():
+   return "Hello World!"
 
-@app.route('/status')
-def status():
-    return jsonify({"status": "running", "message": "App is healthy ✅"})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8080)
